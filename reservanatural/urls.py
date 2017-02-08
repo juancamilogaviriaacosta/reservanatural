f26f6a58c44e54edbacfed3d3059c3f9a7067eb0
juan <jcgaviriaa@zurich.co>
2017-02-08 14:01:35 -0500

    creating static directory

--- reservanatural/urls.py
+++ reservanatural/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^', admin.site.urls),
     url(r'^admin/', admin.site.urls),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
