63f3de134beda8bff9e4d8a80b2d2c4bf136e2cc
ivan-alfonso <ivandaalf@hotmail.com>
2017-02-15 02:33:44 -0500

    Change static files

--- reservanatural/settings.py
+++ reservanatural/settings.py
@@ -120,7 +120,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'static')
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'static’, ‘media’)
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
