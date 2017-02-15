280d50718fbd3484c93c68287c4fc1249a0e3cb2
ivan-alfonso <ivandaalf@hotmail.com>
2017-02-15 02:22:29 -0500

    Change static files

--- reservanatural/settings.py
+++ reservanatural/settings.py
@@ -125,5 +125,4 @@ MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'static', 'media')
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
-    os.path.join(PROJECT_ROOT, 'static’, ‘media’),
 )
