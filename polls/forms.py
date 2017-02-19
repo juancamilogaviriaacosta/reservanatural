72b019d1f3d2ce9f2c67e8b3282bdd69ded31b21
Santiago Puerto <santiago.puerto@gmail.com>
2017-02-19 05:25:27 -0500

    Issuewith spanish accents fixed

--- polls/forms.py
+++ polls/forms.py
@@ -4,18 +4,18 @@ from .models import UserProfile
 
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput, label='Contraseña')
-    password2 = forms.CharField(widget=forms.PasswordInput, label='Confirmar contraseña')
+    password = forms.CharField(widget=forms.PasswordInput, label='Password')
+    password2 = forms.CharField(widget=forms.PasswordInput, label='Confirmar password')
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password',)
         labels = {
             'username': 'Nombre de usuario',
-            'email': 'Correo electrónico',
+            'email': 'Correo electronico',
         }
         help_texts = {
-            'username': 'Este será el código de usuario para iniciar sesión'
+            'username': 'Este sera el cidigo de usuario para iniciar sesion'
         }
 
 
