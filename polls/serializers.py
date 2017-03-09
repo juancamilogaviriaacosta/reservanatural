7d5212bdea62ab4711762bb182b79831ff09cb80
mspuerto <ms.puerto@uniandes.edu.co>
2017-03-09 10:56:48 -0500

    Implemented user comments

--- polls/serializers.py
+++ polls/serializers.py
@@ -2,6 +2,7 @@ from django.core import serializers
 
 class Serializador(object):
 
+    @staticmethod
     def to_json(QuerySet):
         data = serializers.serialize("json", QuerySet)
         return data
